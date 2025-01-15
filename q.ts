a219023d62d6a1ed5e3a5ff9e7915bf628aaa59f
kisesesky <kisesesky19@gmail.com>
2025-01-15 21:07:16 +0900

    0115 study

---
+++ q.ts
@@ -0,0 +1,30 @@
+
+// const li = document.createElement('li') as HTMLElement
+// const inputvalue = document.getElementById('item') as HTMLInputElement
+// li.classList.add("list-group")
+// li.innerHTML = `<button>삭제</button>`
+// const value = inputvalue.value.trim() as string
+// const ul = document.getElementById('items') as HTMLElement
+// ul.appendChild(li)
+
+
+// const itemElement = document.getElementById('item') as HTMLElement
+// const itemValue = itemElement.value
+// const newElement = document.createElement('li')
+// newElement.className = "list-group"
+// newElement.innerText = itemValue
+
+// const deleteBtnElement = document.createElement('button')
+// deleteBtnElement.className = 'dddd'
+// deleteBtnElement.innerText = '삭제'
+
+// newElement.appendChild(deleteBtnElement)
+// itemList.appendChild(newElement)
+
+// itemElement.value = ''
+
+
+// if(e.target instanceof HTMLButtonElement){
+//     if(confirm('delete?'))
+//         itemList.removeChild(e.target.parentElement)
+// }
